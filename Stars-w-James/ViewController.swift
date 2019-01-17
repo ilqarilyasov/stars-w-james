b852e5486d30138186d9ea292c1b0141ab8a5806
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-17 11:09:41 -0600

    Initial Commit

---
+++ Stars-w-James/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars-w-James
+//
+//  Created by Ilgar Ilyasov on 1/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
